84c0ce2d9e13af4c8f33e87da1e66df5b65cf4f2
LAOVA <172326383@qq.com>
2023-05-05 12:28:33 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="bottom">
+        <p>
+            <router-link to="/first">社区管理</router-link>
+            <router-link to="/second">节能分析</router-link>
+        </p>
+    </div>
+    <router-view></router-view>
+</template>
+
+<style scoped>
+#app {
+    position: relative;
+    padding: 0;
+    margin: 0;
+    width: 100%;
+    height: 100vh;
+}
+.bottom {
+    position: absolute;
+    bottom: 50px;
+    left: 50%;
+    transform: translateX(-50%);
+}
+</style>
